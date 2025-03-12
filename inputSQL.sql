48fd1e0bba7ff7efd8e73608e279052e50ed3d06
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-12 14:26:56 +0300

    v3.3

--- inputSQL.sql
+++ inputSQL.sql
@@ -1,4 +1,1 @@
-insert into public.codewars (name_puzzle, point_puzzle, status_puzzle)
-values ('Weight_for_weight', 0, 'N')
-
--- SELECT c.* FROM public.codewars AS c
+SELECT * FROM public.codewars;
